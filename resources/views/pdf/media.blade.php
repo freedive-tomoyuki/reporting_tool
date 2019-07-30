a61e664807b4fb1f990dab61dde23709eb3b5d93
Tomoyuki Sato <t.sato@freedive.co.jp>
2019-07-30 15:30:52 +0900

    （修正）font fix

--- resources/views/pdf/media.blade.php
+++ resources/views/pdf/media.blade.php
@@ -1,5 +1,4 @@
-<!doctype html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <link href="{{ public_path('css/bootstrap.min.css')}}" rel="stylesheet">
