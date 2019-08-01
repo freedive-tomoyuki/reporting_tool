3fb11357acf3e1c1ce942412e65924413c971518
Tomoyuki Sato <t.sato@freedive.co.jp>
2019-08-01 11:06:29 +0900

    upload vender

--- config/app.php
+++ config/app.php
@@ -170,7 +170,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        //App\Providers\TelescopeServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\CSVServiceProvider::class,
         Weidner\Goutte\GoutteServiceProvider::class,
