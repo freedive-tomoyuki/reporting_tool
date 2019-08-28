abe30fa096896a0aff9397b842d595b6025adfe4
Tomoyuki Sato <t.sato@freedive.co.jp>
2019-08-28 19:46:28 +0900

    add queue work

--- app/Http/Controllers/Admin/DailyCrawlerController.php
+++ app/Http/Controllers/Admin/DailyCrawlerController.php
@@ -257,7 +257,7 @@ class DailyCrawlerController extends Controller
     private function refreshQueue():void
     {
         //\Artisan::call('queue:flush');
-        \Artisan::call('queue:work --timeout=0');
+        //\Artisan::call('queue:work --timeout=0');
         //\DB::table('jobs')->truncate();
     }
 
