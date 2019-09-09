a4ddba9e2a5196eca4bda5a3a615723f5e587b09
Tomoyuki Sato <t.sato@freedive.co.jp>
2019-09-09 16:05:12 +0900

    時間変更

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->call('App\Http\Controllers\Admin\DailyCrawlerController@dailytimer')->dailyAt('16:04');
+        $schedule->call('App\Http\Controllers\Admin\DailyCrawlerController@dailytimer')->dailyAt('16:06');
 
         $schedule->call('App\Http\Controllers\Admin\MonthlyCrawlerController@monthlytimer')->dailyAt('16:14');
 
