c7edf2a3c5426cca5430d8b9a8a2d62a8d173617
freedive-tomoyuki <47741936+freedive-tomoyuki@users.noreply.github.com>
2019-12-21 17:03:46 +0900

    Update 2019_04_08_022015_add_products_table.php

--- database/migrations/2019_04_08_022015_add_products_table.php
+++ database/migrations/2019_04_08_022015_add_products_table.php
@@ -16,6 +16,9 @@ class AddProductsTable extends Migration
         Schema::table('products', function (Blueprint $table) {
             $table->BigInteger('product_base_id')->unsigned();
             $table->foreign('product_base_id')->references('id')->on('product_bases');
+            $table->Integer('asp_product_id')->after('password_value')->nullable();
+            $table->Integer('asp_sponsor_id')->after('password_value')->nullable();
+            $table->Integer('price')->nullable()->default(0);
 
         });
     }
